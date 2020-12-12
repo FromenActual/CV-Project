ca5d5ab30c5b18b9c40b0c54a86c73813d09c3d3
Dryw Wade <andryw.wade@gmail.com>
2020-12-11 22:31:10 -0700

    Final edits

--- data_input.py
+++ data_input.py
@@ -94,10 +94,12 @@ for image_file_name in image_file_names:
 	template_directory = "Templates/"
 	template_filename = os.path.join(template_directory, "brace.png")
 	template = cv2.imread(template_filename, 0)
+	templateScaler = (yLines[9]-yLines[0])/157
+	template = cv2.resize(template, (int(template.shape[1]*templateScaler), int(template.shape[0]*templateScaler)))
 	
 	# Use template matching to find templates
 	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
-	thresh = 0.5
+	thresh = 0.7
 	matches = np.where(scores > thresh)
 	
 	# Add templates to new image
@@ -119,10 +121,12 @@ for image_file_name in image_file_names:
 	template_directory = "Templates/"
 	template_filename = os.path.join(template_directory, "treble_clef.png")
 	template = cv2.imread(template_filename, 0)
+	templateScaler = (yLines[4]-yLines[0])/40
+	template = cv2.resize(template, (int(template.shape[1]*templateScaler), int(template.shape[0]*templateScaler)))
 	
 	# Use template matching to find templates
 	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
-	thresh = 0.5
+	thresh = 0.7
 	matches = np.where(scores > thresh)
 	
 	# Add templates to new image
@@ -140,10 +144,12 @@ for image_file_name in image_file_names:
 	template_directory = "Templates/"
 	template_filename = os.path.join(template_directory, "bass_clef.png")
 	template = cv2.imread(template_filename, 0)
+	templateScaler = (yLines[4]-yLines[0])/40
+	template = cv2.resize(template, (int(template.shape[1]*templateScaler), int(template.shape[0]*templateScaler)))
 	
 	# Use template matching to find templates
 	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
-	thresh = 0.5
+	thresh = 0.7
 	matches = np.where(scores > thresh)
 	
 	# Add templates to new image
@@ -178,8 +184,8 @@ for image_file_name in image_file_names:
 			thisClef = clef
 		
 		# Just in case...
-		if(thisClef == ''):
-			raise('CLEF NOT DETECTED')
+		#if(thisClef == ''):
+	#		raise('CLEF NOT DETECTED')
 		
 		if(thisClef == 'treble'):
 			yNotes[int(middle-noteSpacing*9)] = ['D',6]
@@ -272,7 +278,99 @@ for image_file_name in image_file_names:
 	## Detect Rests
 	####################################################################################
 	
-	# Code
+	## Quarter
+	
+	# Open template
+	template_directory = "Templates/"
+	template_filename = os.path.join(template_directory, "rest_quarter.png")
+	template = cv2.imread(template_filename, 0)
+	
+	# Use template matching to find templates
+	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
+	thresh = 0.6
+	matches = np.where(scores > thresh)
+	
+	# Add templates to new image
+	for i in range(len(matches[0])):
+		x = matches[1][i]
+		y = matches[0][i]
+		features_img[y:y+template.shape[0], x:x+template.shape[1]] |= template
+	
+	## Eighth
+	
+	# Open template
+	template_directory = "Templates/"
+	template_filename = os.path.join(template_directory, "rest_eighth.png")
+	template = cv2.imread(template_filename, 0)
+	
+	# Use template matching to find templates
+	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
+	thresh = 0.7
+	matches = np.where(scores > thresh)
+	
+	# Add templates to new image
+	for i in range(len(matches[0])):
+		x = matches[1][i]
+		y = matches[0][i]
+		features_img[y:y+template.shape[0], x:x+template.shape[1]] |= template
+	
+	## Sixteenth
+	
+	# Open template
+	template_directory = "Templates/"
+	template_filename = os.path.join(template_directory, "rest_sixteenth.png")
+	template = cv2.imread(template_filename, 0)
+	
+	# Use template matching to find templates
+	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
+	thresh = 0.8
+	matches = np.where(scores > thresh)
+	
+	# Add templates to new image
+	for i in range(len(matches[0])):
+		x = matches[1][i]
+		y = matches[0][i]
+		features_img[y:y+template.shape[0], x:x+template.shape[1]] |= template
+	
+	####################################################################################
+	## Detect Accidentals
+	####################################################################################
+	
+	## Sharps
+	
+	# Open template
+	template_directory = "Templates/"
+	template_filename = os.path.join(template_directory, "sharp.png")
+	template = cv2.imread(template_filename, 0)
+	
+	# Use template matching to find templates
+	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
+	thresh = 0.5
+	matches = np.where(scores > thresh)
+	
+	# Add templates to new image
+	for i in range(len(matches[0])):
+		x = matches[1][i]
+		y = matches[0][i]
+		features_img[y:y+template.shape[0], x:x+template.shape[1]] |= template
+	
+	## Flats
+	
+	# Open template
+	template_directory = "Templates/"
+	template_filename = os.path.join(template_directory, "flat.png")
+	template = cv2.imread(template_filename, 0)
+	
+	# Use template matching to find templates
+	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
+	thresh = 0.7
+	matches = np.where(scores > thresh)
+	
+	# Add templates to new image
+	for i in range(len(matches[0])):
+		x = matches[1][i]
+		y = matches[0][i]
+		features_img[y:y+template.shape[0], x:x+template.shape[1]] |= template
 	
 	####################################################################################
 	## Detect Short Staff Lines
@@ -361,7 +459,7 @@ for image_file_name in image_file_names:
 	
 	# Use template matching to find templates
 	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
-	thresh = 0.5
+	thresh = 0.6
 	matches = np.where(scores > thresh)
 	
 	# Add templates to new image
@@ -372,6 +470,30 @@ for image_file_name in image_file_names:
 		
 		# Save duration of this note for later
 		noteBeats.append([int(x+template.shape[1]/2), int(y+template.shape[0]/2), 2])
+	
+	# Remove these notes from image
+	bin_img = cv2.bitwise_and(bin_img, cv2.bitwise_not(notes_img))
+	
+	## Detect eighth notes
+	
+	# Open template
+	template_directory = "Templates/"
+	template_filename = os.path.join(template_directory, "note_eighth.png")
+	template = cv2.imread(template_filename, 0)
+	
+	# Use template matching to find templates
+	scores = cv2.matchTemplate(bin_img, template, cv2.TM_CCOEFF_NORMED)
+	thresh = 0.82
+	matches = np.where(scores > thresh)
+	
+	# Add templates to new image
+	for i in range(len(matches[0])):
+		x = matches[1][i]
+		y = matches[0][i]
+		notes_img[y:y+template.shape[0], x:x+template.shape[1]] |= template
+		
+		# Save duration of this note for later
+		noteBeats.append([int(x+8), int(y+4), 8])
 	
 	# Remove these notes from image
 	bin_img = cv2.bitwise_and(bin_img, cv2.bitwise_not(notes_img))
@@ -451,17 +573,11 @@ for image_file_name in image_file_names:
 		# Sort notes by x value to order sequentially
 		noteLocationGroups[i] = noteLocationGroups[i][np.argsort(noteLocationGroups[i][:,0])]
 	
-	
-	
-	
-	
-	
-	
-	
 	####################################################################################
 	## Create Song
 	####################################################################################
 	
+	'''# Section only works for MHALL
 	# Song info
 	numBeats = 32
 	bpm = 180
@@ -533,7 +649,7 @@ for image_file_name in image_file_names:
 	song = song.astype(np.int16)
 	
 	import scipy.io.wavfile
-	scipy.io.wavfile.write("song.wav", samplingRate, song)
+	scipy.io.wavfile.write("song.wav", samplingRate, song)'''
 	
 	while(True):
 		if(cv2.waitKey(0) != ord('p')):
@@ -544,134 +660,3 @@ for image_file_name in image_file_names:
 		# wait for playback to finish before exiting
 		play_obj.wait_done()
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
-	# ############################################################################
-	# #Does the hough transform stuff. does not work well
-	# thresh_canny=5
-	# MIN_FRACT_EDGES = 0.05
-	# MAX_FRACT_EDGES = 0.1
-	# edge_img = cv2.Canny(
-	# 	image=gray_img,
-	# 	apertureSize=3,  # size of Sobel operator
-	# 	threshold1=thresh_canny,  # lower threshold
-	# 	threshold2=3 * thresh_canny,  # upper threshold
-	# 	L2gradient=True)  # use more accurate L2 norm
-	# while np.sum(edge_img)/255 < MIN_FRACT_EDGES * (image_width * image_height):
-	# 	thresh_canny *= 0.9
-	# 	edge_img = cv2.Canny(
-	# 		image=gray_img,apertureSize=3,  # size of Sobel operator
-	# 		threshold1=thresh_canny,  # lower threshold
-	# 		threshold2=3 * thresh_canny,  # upper threshold
-	# 		L2gradient=True)  # use more accurate L2 norm
-	# while np.sum(edge_img)/255 > MAX_FRACT_EDGES * (image_width * image_height):
-	# 	thresh_canny *= 1.1
-	# 	edge_img = cv2.Canny(image=gray_img,apertureSize=3,  # size of Sobel operator
-	# 	threshold1=thresh_canny,  # lower threshold
-	# 	threshold2=3 * thresh_canny,  # upper threshold
-	# 	L2gradient=True)  # use more accurate L2 norm
-	# cv2.imshow('Edge Image',edge_img)
-	# MIN_HOUGH_VOTES_FRACTION=.05
-	# MIN_LINE_LENGTH_FRACTION=.05
-	# hougstaffLines = cv2.HougstaffLinesP(
-	#     image=edge_img,
-	#     rho=1,
-	#     theta=np.pi/180,
-	#     threshold=int(image_width * MIN_HOUGH_VOTES_FRACTION),
-	#     lines=None,
-	#     minLineLength=int(image_width * MIN_LINE_LENGTH_FRACTION),
-	#     )
-	# print("Found %d line segments" % len(hougstaffLines))
-
-	# # For visualizing the lines, draw on a grayscale version of the image.
-	# for i in range(0, len(hougstaffLines)):
-	#     l = hougstaffLines[i][0]
-	#     cv2.line(org_img, (l[0], l[1]), (l[2], l[3]), (0, 255, 255), 
-	#              thickness=2, lineType=cv2.LINE_AA)
-	# cv2.imshow("Hough linesP", org_img)
-	# org_img_copy = org_img.copy()
-	# vanishing.find_vanishing_point_directions(hougstaffLines, org_img_copy, num_to_find=3, K=None)
-	# cv2.imshow("overlay", org_img_copy)
-
-	# ############################################################################
-	# # https://docs.opencv.org/3.4/dd/dd7/tutorial_morph_lines_detection.html
-
-
-	# horizontal = np.copy(edge_img)
-	# vertical = np.copy(edge_img)
-	# # [init]
-	# # [horiz]
-	# # Specify size on horizontal axis
-	# cols = horizontal.shape[1]
-	# horizontal_size = cols // 30
-	# # Create structure element for extracting horizontal lines through morphology operations
-	# horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (horizontal_size, 1))
-	# # Apply morphology operations
-	# horizontal = cv2.erode(horizontal, horizontalStructure)
-	# horizontal = cv2.dilate(horizontal, horizontalStructure)
-	# # Show extracted horizontal lines
-	# rows = vertical.shape[0]
-	# verticalsize = rows // 30
-	# # Create structure element for extracting vertical lines through morphology operations
-	# verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, verticalsize))
-	# # Apply morphology operations
-	# vertical = cv2.erode(vertical, verticalStructure)
-	# vertical = cv2.dilate(vertical, verticalStructure)
-	# # Show extracted vertical lines
-	# cv2.imshow("vertical", vertical)
-	# # [vert]
-	# # [smooth]
-	# # Inverse vertical image
-	# vertical = cv2.bitwise_not(vertical)
-	# cv2.imshow("vertical_bit", horizontal)
-	# '''
-	# Extract edges and smooth image according to the logic
-	# 1. extract edges
-	# 2. dilate(edges)
-	# 3. src.copyTo(smooth)
-	# 4. blur smooth img
-	# 5. smooth.copyTo(src, edges)
-	# '''
-	# # Step 1
-	# edges = cv2.adaptiveThreshold(vertical, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
-	#                             cv2.THRESH_BINARY, 3, -2)
-	# cv2.imshow("edges", edges)
-	# # Step 2
-	# kernel = np.ones((2, 2), np.uint8)
-	# edges = cv2.dilate(edges, kernel)
-	# cv2.imshow("dilate", edges)
-	# # Step 3
-	# smooth = np.copy(vertical)
-	# # Step 4
-	# smooth = cv2.blur(smooth, (2, 2))
-	# # Step 5
-	# (rows, cols) = np.where(edges != 0)
-	# vertical[rows, cols] = smooth[rows, cols]
-	# cv2.imshow("horizontal", vertical)
-	# ############################################################################
-	# #This shows promise
-	# size=2
-	# kernel = kernel = np.ones((size, size), np.uint8)
-	# horizontal = cv2.morphologyEx(horizontal, cv2.MORPH_CLOSE, kernel)
-	# horizontal = cv2.morphologyEx(horizontal, cv2.MORPH_OPEN, kernel)
-	# cv2.imshow('test',horizontal)
-
-
